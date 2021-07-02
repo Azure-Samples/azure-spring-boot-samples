870b9dd32d2b301de577f6e66a8c670d28ad19d3
zhihaoguo <zhihaoguo@microsoft.com>
2021-07-02 17:44:00 +0800

    - test github action

--- aad/azure-spring-boot-sample-active-directory-b2c-oidc/src/main/java/com/azure/spring/sample/aad/b2c/AzureADB2COIDCSampleApplication.java
+++ aad/azure-spring-boot-sample-active-directory-b2c-oidc/src/main/java/com/azure/spring/sample/aad/b2c/AzureADB2COIDCSampleApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzureADB2COIDCSampleApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(AzureADB2COIDCSampleApplication.class, args);
     }
 }

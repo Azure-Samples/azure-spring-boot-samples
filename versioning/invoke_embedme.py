fda0a4ad6f8433f5361ee1c95645280cfc689cbd
zhihaoguo <zhihaoguo@microsoft.com>
2021-07-06 13:45:26 +0800

    add file invoke_embedme.py

--- versioning/invoke_embedme.py
+++ versioning/invoke_embedme.py
@@ -8,13 +8,24 @@
 import argparse
 import os
 import sys
+import urllib.request
 
 # NPX command.
 npx_command = 'npx embedme'
 
-
+def delete_from_file(the_file):
+    if os.path.exists(the_file):
+      os.remove(the_file)
 
 def main():
+    # load file from azure-sdk-for-java
+    keyVaultJcaManagedIdentitySample_path = os.path.abspath("../keyvault/azure-spring-boot-sample-keyvault-certificates-client-side/src/main/java/com/azure/spring/security/keyvault/certificates/KeyVaultJcaManagedIdentitySample.java")
+    url = r"https://raw.githubusercontent.com/Azure/azure-sdk-for-java/main/sdk/spring/azure-spring-boot/src/samples/java/com/azure/spring/keyvault/KeyVaultJcaManagedIdentitySample.java"
+    content_from_url =  urllib.request.urlopen(url).read()
+
+    with open(keyVaultJcaManagedIdentitySample_path, "wb+" ) as f:
+        f.write(content_from_url)
+
     command = npx_command
 
     readmePath = "../keyvault/azure-spring-boot-sample-keyvault-certificates-client-side/README.md"
@@ -22,7 +33,11 @@ def main():
     print(os.path.abspath(readmePath))
     command += ' ' + os.path.abspath(readmePath)
 
-    sys.exit(os.system(command))
+    os.system(command)
+
+    delete_from_file(keyVaultJcaManagedIdentitySample_path)
+
+    sys.exit()
 
 if __name__ == '__main__':
     main()

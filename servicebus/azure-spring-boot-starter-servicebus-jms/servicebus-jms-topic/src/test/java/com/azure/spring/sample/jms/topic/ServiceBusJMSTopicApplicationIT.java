8f2d369484bb531c094a845282ef2eed015e8dc3
zhihaoguo <zhihaoguo@microsoft.com>
2021-07-27 10:03:26 +0800

    - rename modules in servicebus

--- servicebus/azure-spring-boot-starter-servicebus-jms/servicebus-jms-topic/src/test/java/com/azure/spring/sample/jms/topic/ServiceBusJMSTopicApplicationIT.java
+++ servicebus/azure-spring-boot-starter-servicebus-jms/servicebus-jms-topic/src/test/java/com/azure/spring/sample/jms/topic/ServiceBusJMSTopicApplicationIT.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
-import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 

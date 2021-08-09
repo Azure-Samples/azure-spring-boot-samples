5a34a54f2fd43a735883e83dc43a345924724141
michaelqi793 <78671298+michaelqi793@users.noreply.github.com>
2021-08-09 10:11:42 +0800

    Skip the role assignment when creating resources and fix the relative script path (#23)

--- keyvault/azure-spring-boot-starter-keyvault-certificates/keyvault-certificates-client-side/script/setup.sh
+++ keyvault/azure-spring-boot-starter-keyvault-certificates/keyvault-certificates-client-side/script/setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-source ../azure-spring-boot-starter-keyvault-certificates/keyvault-certificates-server-side/script/export_environment_variables.sh
-source ../azure-spring-boot-starter-keyvault-certificates/keyvault-certificates-server-side/script/export_environment_variables_of_created_resource.sh
+source ../keyvault-certificates-server-side/script/export_environment_variables.sh
+source ../keyvault-certificates-server-side/script/export_environment_variables_of_created_resource.sh
--- keyvault/azure-spring-boot-starter-keyvault-certificates/keyvault-certificates-server-side/script/export_environment_variables_of_created_resource.sh
+++ keyvault/azure-spring-boot-starter-keyvault-certificates/keyvault-certificates-server-side/script/export_environment_variables_of_created_resource.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 export KEY_VAULT_URI=
 

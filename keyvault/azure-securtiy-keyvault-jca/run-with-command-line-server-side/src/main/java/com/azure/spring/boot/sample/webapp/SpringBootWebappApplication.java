4603c4b221180505f7cb8e6d4fae975cd23124b8
michaelqi793 <archer@Archers-MacBook-Pro.local>
2021-08-11 17:30:09 +0800

    Server side structure

---
+++ keyvault/azure-securtiy-keyvault-jca/run-with-command-line-server-side/src/main/java/com/azure/spring/boot/sample/webapp/SpringBootWebappApplication.java
@@ -0,0 +1,13 @@
+package com.azure.spring.boot.sample.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebappApplication.class, args);
+	}
+
+}

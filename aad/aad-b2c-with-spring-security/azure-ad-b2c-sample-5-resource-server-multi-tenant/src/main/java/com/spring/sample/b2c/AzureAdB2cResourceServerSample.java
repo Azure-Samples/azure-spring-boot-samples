f6214845e0f4a34d1e3cc91a169fc14faae25211
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2021-10-12 17:54:34 +0800

    1.update package and module name 2.delete and update some error contents

---
+++ aad/aad-b2c-with-spring-security/azure-ad-b2c-sample-5-resource-server-multi-tenant/src/main/java/com/spring/sample/b2c/AzureAdB2cResourceServerSample.java
@@ -0,0 +1,15 @@
+package com.spring.sample.b2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring application.
+ */
+@SpringBootApplication
+public class AzureAdB2cResourceServerSample {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AzureAdB2cResourceServerSample.class, args);
+  }
+}

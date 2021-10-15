fd0a6321c0d0d76d655dd006ce31c1542e65da47
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2021-10-15 10:49:30 +0800

    fix error which will not verify scope

--- aad/aad-b2c-with-spring-security/sample-02-web-application-authorize-by-scope/src/main/java/com/spring/sample/b2c/security/WebSecurityConfiguration.java
+++ aad/aad-b2c-with-spring-security/sample-02-web-application-authorize-by-scope/src/main/java/com/spring/sample/b2c/security/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.spring.sample.b2c.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * Security Configuration.
  */
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Value("${logout-success-url}")

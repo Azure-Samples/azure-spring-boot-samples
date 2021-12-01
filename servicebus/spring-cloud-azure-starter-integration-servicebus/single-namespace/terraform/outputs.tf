fe902b319fa7d2b99f386786759f56392555da1f
zhihaoguo <zhihaoguo@microsoft.com>
2021-12-01 20:58:32 +0800

    add servicebus namespace output

--- servicebus/spring-cloud-azure-starter-integration-servicebus/single-namespace/terraform/outputs.tf
+++ servicebus/spring-cloud-azure-starter-integration-servicebus/single-namespace/terraform/outputs.tf
@@ -2,3 +2,9 @@ output "resource_group" {
   value       = azurerm_resource_group.main.name
   description = "The resource group."
 }
+
+
+output "servicebus_namespace" {
+  value       = azurerm_servicebus_namespace.application.name
+  description = "The servicebus namespace."
+}

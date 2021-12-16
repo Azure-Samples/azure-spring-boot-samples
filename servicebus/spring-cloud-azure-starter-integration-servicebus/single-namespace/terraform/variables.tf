2bec710b0c0939eefe4b821d38f2afe846b850d4
zhihaoguo <zhihaoguo@microsoft.com>
2021-12-16 16:29:42 +0800

    remove unused variables

--- servicebus/spring-cloud-azure-starter-integration-servicebus/single-namespace/terraform/variables.tf
+++ servicebus/spring-cloud-azure-starter-integration-servicebus/single-namespace/terraform/variables.tf
@@ -4,18 +4,6 @@ variable "application_name" {
   default     = "servicebusapp"
 }
 
-variable "service_principal_id" {
-  type        = string
-  description = "The Azure Service Principal object_id to assign role to, not the application id here"
-  default     = "8906e162-4967-4e8f-84f1-1e7bccdf93c2"
-}
-
-variable "environment" {
-  type        = string
-  description = "The environment (dev, test, prod...)"
-  default     = "dev"
-}
-
 variable "location" {
   type        = string
   description = "The Azure region where all resources in this example should be created"

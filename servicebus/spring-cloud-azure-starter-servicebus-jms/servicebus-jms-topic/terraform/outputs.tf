37be5ff3e291df2e7d6715729a48a8ebebd8d8a5
zhihaoguo <zhihaoguo@microsoft.com>
2021-12-31 08:39:29 +0800

    add sensitive = true

--- servicebus/spring-cloud-azure-starter-servicebus-jms/servicebus-jms-topic/terraform/outputs.tf
+++ servicebus/spring-cloud-azure-starter-servicebus-jms/servicebus-jms-topic/terraform/outputs.tf
@@ -1,4 +1,5 @@
 output "CONNECTION_STRING" {
   value = azurerm_servicebus_namespace.servicebus_namespace.default_primary_connection_string
   description = "The connection_string of servicebus namespace."
+  sensitive = true
 }

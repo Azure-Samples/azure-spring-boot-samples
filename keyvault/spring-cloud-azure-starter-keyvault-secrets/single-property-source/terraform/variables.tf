c8405741e865db1dcfc173f925cc8ef3701ec940
zhihaoguo <zhihaoguo@microsoft.com>
2022-01-04 13:29:51 +0800

    the max length of "azurerm_key_vault" is 24

--- keyvault/spring-cloud-azure-starter-keyvault-secrets/single-property-source/terraform/variables.tf
+++ keyvault/spring-cloud-azure-starter-keyvault-secrets/single-property-source/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "application_name" {
   type        = string
   description = "The name of your application."
-  default     = "single-property-keyvault"
+  default     = "keyvault"
 }
 
 variable "location" {

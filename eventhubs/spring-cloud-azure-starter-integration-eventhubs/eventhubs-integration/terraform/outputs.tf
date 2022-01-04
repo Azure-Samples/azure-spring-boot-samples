077a9d34a8e6e8842bc9ebbfe2b03fe8924b6bad
zhihaoguo <zhihaoguo@microsoft.com>
2022-01-04 13:43:54 +0800

    refactor eventhubs samples with terraform and defaultcredential

---
+++ eventhubs/spring-cloud-azure-starter-integration-eventhubs/eventhubs-integration/terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "AZURE_EVENTHUBS_NAMESPACE" {
+  value       = azurerm_eventhub_namespace.eventhubs_namespace.name
+  description = "The event hubs namespace."
+}
+
+output "AZURE_STORAGE_ACCOUNT_NAME" {
+  value       = azurerm_storage_account.storage_account.name
+  description = "The storage account name."
+}
+
+output "AZURE_STORAGE_CONTAINER_NAME" {
+  value       = azurerm_storage_container.storage_container.name
+  description = "The container name created in storage account."
+}

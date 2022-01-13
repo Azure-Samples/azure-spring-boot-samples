ccabc1a8da057edf479388e45876343d6f8d9643
zhihaoguo <zhihaoguo@microsoft.com>
2022-01-13 15:37:47 +0800

    Refactor samples with terraform and defaultCredential for Storage Queue samples (#139)

--- storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-integration/terraform/variables.tf
+++ storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-integration/terraform/variables.tf
@@ -1,19 +1,13 @@
 variable "application_name" {
   type        = string
   description = "The name of your application."
-  default     = "blob-sample"
+  default     = "integration"
 }
 
 variable "location" {
   type        = string
   description = "The Azure region where all resources in this example should be created."
   default     = "eastus"
-}
-
-variable "container_name" {
-  type        = string
-  description = "The container name of the blob storage"
-  default     = "blobcontainer"
 }
 
 variable "sample_tag_value" {
---
+++ storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-operation/terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "ACCOUNT_NAME" {
+  value       = azurerm_storage_account.storage_account.name
+  description = "The name of storage account."
+}

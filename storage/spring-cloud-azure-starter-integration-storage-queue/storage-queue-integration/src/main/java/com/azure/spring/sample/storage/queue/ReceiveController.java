ce8dacfbdbbf14d3049b8e23c8f267035296649a
zhihaoguo <zhihaoguo@microsoft.com>
2022-01-20 16:00:28 +0800

    update broken links

--- storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-integration/src/main/java/com/azure/spring/sample/storage/queue/ReceiveController.java
+++ storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-integration/src/main/java/com/azure/spring/sample/storage/queue/ReceiveController.java
@@ -7,7 +7,7 @@ import com.azure.spring.integration.storage.queue.inbound.StorageQueueMessageSou
 import com.azure.spring.messaging.AzureHeaders;
 import com.azure.spring.messaging.checkpoint.CheckpointMode;
 import com.azure.spring.messaging.checkpoint.Checkpointer;
-import com.azure.spring.storage.queue.core.StorageQueueOperation;
+import com.azure.spring.storage.queue.core.StorageQueueTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -31,8 +31,8 @@ public class ReceiveController {
 
     @Bean
     @InboundChannelAdapter(channel = INPUT_CHANNEL, poller = @Poller(fixedDelay = "1000"))
-    public StorageQueueMessageSource storageQueueMessageSource(StorageQueueOperation storageQueueOperation) {
-        storageQueueOperation.setCheckpointMode(CheckpointMode.MANUAL);
+    public StorageQueueMessageSource storageQueueMessageSource(StorageQueueTemplate storageQueueOperation) {
+
 
         return new StorageQueueMessageSource(STORAGE_QUEUE_NAME, storageQueueOperation);
     }

a4307058f84a2f03be546929b8e53797a88303a2
zhihaoguo <zhihaoguo@microsoft.com>
2022-01-26 13:30:06 +0800

    Update Storageblob sample (#167)

--- storage/spring-cloud-azure-starter-storage-blob/storage-blob-sample/src/main/java/com/azure/spring/sample/storage/resource/BlobController.java
+++ storage/spring-cloud-azure-starter-storage-blob/storage-blob-sample/src/main/java/com/azure/spring/sample/storage/resource/BlobController.java
@@ -3,10 +3,14 @@
 
 package com.azure.spring.sample.storage.resource;
 
+import com.azure.spring.core.resource.AzureStorageBlobProtocolResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.WritableResource;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +28,55 @@ public class BlobController {
 
     final static Logger logger = LoggerFactory.getLogger(BlobController.class);
 
-    public BlobController() {
-        logger.info("spring.cloud.azure.storage.blob.endpoint configured");
-    }
-
     @Value("${resource.blob}")
     private Resource azureBlobResource;
+
+    @Value("${storage-container-name}")
+    private String containerName;
+
+    @Autowired
+    @Qualifier("webApplicationContext")
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private AzureStorageBlobProtocolResolver azureStorageBlobProtocolResolver;
+
+    /**
+     * Using resourceLoader to get Azure Storage Blob resource with filename.
+     *
+     * @param fileName the fileName(contains extension name) stored in Storage Blob Container.
+     *                 eg: fileName = fileName1.txt
+     * @return the content stored in the file.
+     * @throws IOException
+     */
+    @GetMapping("/getResourceWithResourceLoader/{fileName}")
+    public String getResourceWithResourceLoader(@PathVariable String fileName) throws IOException {
+        // get a BlobResource
+        Resource storageBlobResource = resourceLoader.getResource("azure-blob://" + containerName + "/" + fileName);
+        String fileContent = StreamUtils.copyToString(
+                storageBlobResource.getInputStream(),
+                Charset.defaultCharset());
+        logger.info("Blob content retrieved: fileName={}, fileContent={}", fileName, fileContent);
+        return "fileContent=" + fileContent;
+    }
+
+    /**
+     * Using AzureStorageBlobProtocolResolver to get Azure Storage Blob resources with file pattern.
+     *
+     * @return fileNames in the container match pattern: *.txt
+     * @throws IOException
+     */
+    @GetMapping("/getFileNamesWithProtocolResolver")
+    public String getFileNamesWithProtocolResolver() throws IOException {
+        Resource[] resources = azureStorageBlobProtocolResolver.getResources("azure-blob://" + containerName + "/*.txt");
+        logger.info("{} resources founded with pattern:*.txt",resources.length);
+        StringBuffer sb = new StringBuffer();
+        for (Resource resource : resources) {
+            sb.append(resource.getFilename())
+                    .append("\n");
+        }
+        return sb.toString();
+    }
 
     @GetMapping
     public String readBlobResource() throws IOException {

3e14a90f491150e1c17373947d3efb870ffbddb9
zhihaoguo <zhihaoguo@microsoft.com>
2022-01-27 14:44:21 +0800

    - clean unused imports - renames

--- storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-integration/src/main/java/com/azure/spring/sample/storage/queue/ReceiveService.java
+++ storage/spring-cloud-azure-starter-integration-storage-queue/storage-queue-integration/src/main/java/com/azure/spring/sample/storage/queue/ReceiveService.java
@@ -15,15 +15,15 @@ import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Miao Cao
  */
-@RestController
-public class ReceiveController {
+@Service
+public class ReceiveService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ReceiveController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReceiveService.class);
     /*Storage queue name can only be made up of lowercase letters, the numbers and the hyphen(-).*/
     private static final String STORAGE_QUEUE_NAME = "example";
     private static final String INPUT_CHANNEL = "input";

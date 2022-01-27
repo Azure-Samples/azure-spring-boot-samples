d54fca7d9005bab84d72ce5a1604875e7ee8a5c0
yonghuiye <hui1110rui@163.com>
2022-01-27 14:00:08 +0800

    Add newline.

--- cosmos/spring-cloud-azure-starter-cosmos/spring-cloud-azure-cosmos-sample/terraform/setup_env.sh
+++ cosmos/spring-cloud-azure-starter-cosmos/spring-cloud-azure-cosmos-sample/terraform/setup_env.sh
@@ -9,6 +9,8 @@ writeOnlyRoleDefinitionId=$(terraform -chdir=./terraform output -raw cosmos_appl
 # https://docs.microsoft.com/azure/cosmos-db/how-to-setup-rbac#using-the-azure-cli-1
 az cosmosdb sql role assignment create --account-name $azure_cosmos_account --resource-group $resourceGroupName --scope "/" --principal-id $principalId --role-definition-id $readOnlyRoleDefinitionId
 # assign current Cosmos DB Built-in Data Contributor
+
 az cosmosdb sql role assignment create --account-name $azure_cosmos_account --resource-group $resourceGroupName --scope "/" --principal-id $principalId --role-definition-id $writeOnlyRoleDefinitionId
+
 echo AZURE_COSMOS_ENDPOINT=$AZURE_COSMOS_ENDPOINT
 

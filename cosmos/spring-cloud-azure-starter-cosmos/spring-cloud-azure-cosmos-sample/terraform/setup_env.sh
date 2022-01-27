d44c30bd481a38f3499ff6a8ba5f7a0b5d888e16
yonghuiye <hui1110rui@163.com>
2022-01-27 13:57:29 +0800

    Add newline.

--- cosmos/spring-cloud-azure-starter-cosmos/spring-cloud-azure-cosmos-sample/terraform/setup_env.sh
+++ cosmos/spring-cloud-azure-starter-cosmos/spring-cloud-azure-cosmos-sample/terraform/setup_env.sh
@@ -11,3 +11,4 @@ az cosmosdb sql role assignment create --account-name $azure_cosmos_account --re
 # assign current Cosmos DB Built-in Data Contributor
 az cosmosdb sql role assignment create --account-name $azure_cosmos_account --resource-group $resourceGroupName --scope "/" --principal-id $principalId --role-definition-id $writeOnlyRoleDefinitionId
 echo AZURE_COSMOS_ENDPOINT=$AZURE_COSMOS_ENDPOINT
+

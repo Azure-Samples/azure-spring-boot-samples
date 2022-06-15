39bd0908b19b695c7a7c6615a9f00de3da13db41
Matt Metcalf <mrm9084@gmail.com>
2022-06-15 10:52:47 -0700

    Updating to new directory format

--- appconfiguration/azure-spring-cloud-feature-management/dynamic-features-sample/src/main/java/com/azure/spring/dynamicfeatures/ApplicationConfiguration.java
+++ appconfiguration/azure-spring-cloud-feature-management/dynamic-features-sample/src/main/java/com/azure/spring/dynamicfeatures/ApplicationConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-import com.azure.spring.cloud.feature.manager.feature.evaluators.TargetingEvaluator;
+import com.azure.spring.cloud.feature.manager.feature.filters.TargetingEvaluator;
 import com.azure.spring.cloud.feature.manager.targeting.TargetingEvaluationOptions;
 import com.azure.spring.dynamicfeatures.implementation.TargetingContextImpl;
 

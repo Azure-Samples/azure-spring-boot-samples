9a5e2fbdcb32708d1de7d4e4e97eac04889ddc45
Xiaolu Dai <31124698+saragluna@users.noreply.github.com>
2022-07-14 09:52:57 +0800

    fix the incompatibility between az-cli and terraform (#435)

--- servicebus/spring-cloud-azure-stream-binder-servicebus/servicebus-queue-binder-arm/terraform/main.tf
+++ servicebus/spring-cloud-azure-stream-binder-servicebus/servicebus-queue-binder-arm/terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.99.0"
+      version = "3.9.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"

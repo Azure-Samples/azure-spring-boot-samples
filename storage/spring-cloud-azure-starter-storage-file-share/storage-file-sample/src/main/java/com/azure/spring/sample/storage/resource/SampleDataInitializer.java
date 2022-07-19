0bd3d046cbd9d8800706b2b35d3f2e100ebf03c9
Xiaolu Dai <31124698+saragluna@users.noreply.github.com>
2022-07-19 10:34:41 +0800

    Update SampleDataInitializer.java

--- storage/spring-cloud-azure-starter-storage-file-share/storage-file-sample/src/main/java/com/azure/spring/sample/storage/resource/SampleDataInitializer.java
+++ storage/spring-cloud-azure-starter-storage-file-share/storage-file-sample/src/main/java/com/azure/spring/sample/storage/resource/SampleDataInitializer.java
@@ -17,7 +17,7 @@ import java.io.OutputStream;
 @Component
 public class SampleDataInitializer implements CommandLineRunner {
     static final String FILE_RESOURCE_PATTERN = "azure-file://%s/%s";
-    private final static Logger logger = LoggerFactory.getLogger(SampleDataInitializer.class);
+    private static final Logger logger = LoggerFactory.getLogger(SampleDataInitializer.class);
     private final ResourceLoader resourceLoader;
     private final String shareName;
 

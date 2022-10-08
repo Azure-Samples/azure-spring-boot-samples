a18f9245b5bca45dad3959e63f2ee4cac8e984f7
Shili Chen <v-shilichen@microsoft.com>
2022-10-08 17:47:30 +0800

    Delete unsupported samples

--- spring-petclinic-microservices/spring-petclinic-api-gateway/src/main/resources/static/scripts/owner-details/owner-details.component.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ownerDetails')
-    .component('ownerDetails', {
-        templateUrl: 'scripts/owner-details/owner-details.template.html',
-        controller: 'OwnerDetailsController'
-    });

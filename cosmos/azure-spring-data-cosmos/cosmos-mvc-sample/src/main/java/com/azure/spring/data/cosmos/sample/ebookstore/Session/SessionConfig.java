6686fe8f5d02635586bdf1c8d4755f28088d27f1
Theo van Kraay <thvankra@microsoft.com>
2022-11-17 22:28:48 +0000

    mvc changes

--- cosmos/azure-spring-data-cosmos/cosmos-mvc-sample/src/main/java/com/azure/spring/data/cosmos/sample/ebookstore/Session/SessionConfig.java
+++
@@ -1,8 +0,0 @@
-package com.azure.spring.data.cosmos.sample.ebookstore.Session;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-@Configuration
-public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
-}
--- cosmos/azure-spring-data-cosmos/cosmos-mvc-sample/src/test/java/com/spring/cosmos/ebookstore/StoreApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.spring.cosmos.ebookstore;
-
-import org.junit.jupiter.api.Test;
-
-class StoreApplicationTests {
-	@Test
-	void contextLoads() {
-	}
-}

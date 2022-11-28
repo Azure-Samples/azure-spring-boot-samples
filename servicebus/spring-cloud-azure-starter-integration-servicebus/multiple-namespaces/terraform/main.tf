7fcc77e94a2ecdf333e9b445d2b7b0e8a5cd5c79
Moary Chen <v-shilichen@microsoft.com>
2022-11-28 10:48:59 +0800

    Sync the necessary changes from feature/spring-boot-3 branch (#586)

--- servicebus/spring-cloud-azure-starter-integration-servicebus/multiple-namespaces/terraform/main.tf
+++ servicebus/spring-cloud-azure-starter-integration-servicebus/multiple-namespaces/terraform/main.tf
@@ -38,6 +38,7 @@ resource "azurecaf_name" "servicebus_01" {
   name          = var.application_name
   resource_type = "azurerm_servicebus_namespace"
   random_length = 5
+  random_seed = 1
   clean_input   = true
 }
 

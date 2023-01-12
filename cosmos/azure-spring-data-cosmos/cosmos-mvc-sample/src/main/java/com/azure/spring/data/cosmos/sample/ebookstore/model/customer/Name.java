ebf55adffa953e5505654f1752181579c2a4b277
Theo van Kraay <theo.van@microsoft.com>
2023-01-12 15:25:41 +0000

    requires default constructor

--- cosmos/azure-spring-data-cosmos/cosmos-mvc-sample/src/main/java/com/azure/spring/data/cosmos/sample/ebookstore/model/customer/Name.java
+++ cosmos/azure-spring-data-cosmos/cosmos-mvc-sample/src/main/java/com/azure/spring/data/cosmos/sample/ebookstore/model/customer/Name.java
@@ -7,7 +7,8 @@ import org.apache.commons.lang3.StringUtils;
 public class Name {
     private String firstName;
     private String lastName;
-
+    public Name() {
+    }
     public Name(String firstName, String lastName) {
         this.firstName = StringUtils.capitalize(firstName);
         this.lastName = StringUtils.capitalize(lastName);

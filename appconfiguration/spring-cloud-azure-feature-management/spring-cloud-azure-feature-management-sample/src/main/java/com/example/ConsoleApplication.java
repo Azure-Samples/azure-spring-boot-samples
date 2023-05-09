5a0d024ce0fdbf81ededa31c637e9d0f05bb2e9b
Muyao <v-muyaofeng@microsoft.com>
2023-05-09 16:52:00 +0800

    update f-m sample

--- appconfiguration/spring-cloud-azure-feature-management/spring-cloud-azure-feature-management-sample/src/main/java/com/example/ConsoleApplication.java
+++ appconfiguration/spring-cloud-azure-feature-management/spring-cloud-azure-feature-management-sample/src/main/java/com/example/ConsoleApplication.java
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 package com.example;
 
-import com.azure.spring.cloud.feature.manager.FeatureManager;
+import com.azure.spring.cloud.feature.management.FeatureManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;

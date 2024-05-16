b6005d55ebacc81bdb76bab55485c5b1661ea7d1
Muyao <v-muyaofeng@microsoft.com>
2024-05-16 16:29:21 +0800

    fix

--- servicebus/spring-cloud-azure-starter-servicebus/servicebus-topic/src/main/java/com/azure/spring/sample/jms/topic/ServiceBusProcessorClientConfiguration.java
+++ servicebus/spring-cloud-azure-starter-servicebus/servicebus-topic/src/main/java/com/azure/spring/sample/jms/topic/ServiceBusProcessorClientConfiguration.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.spring.sample.jms.topic;
 
 import com.azure.messaging.servicebus.ServiceBusReceivedMessage;

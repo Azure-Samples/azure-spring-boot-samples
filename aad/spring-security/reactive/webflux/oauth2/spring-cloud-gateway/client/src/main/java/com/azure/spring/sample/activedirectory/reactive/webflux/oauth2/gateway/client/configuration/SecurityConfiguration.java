1cdf092489d8c095b21e0fe0e37e7602df64a579
Muyao <v-muyaofeng@microsoft.com>
2025-01-08 10:21:38 +0800

    update reactive sample

--- aad/spring-security/reactive/webflux/oauth2/spring-cloud-gateway/client/src/main/java/com/azure/spring/sample/activedirectory/reactive/webflux/oauth2/gateway/client/configuration/SecurityConfiguration.java
+++ aad/spring-security/reactive/webflux/oauth2/spring-cloud-gateway/client/src/main/java/com/azure/spring/sample/activedirectory/reactive/webflux/oauth2/gateway/client/configuration/SecurityConfiguration.java
@@ -1,16 +1,19 @@
 package com.azure.spring.sample.activedirectory.reactive.webflux.oauth2.gateway.client.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
+@Configuration
 public class SecurityConfiguration {
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.oauth2Client();
+        http.oauth2Client(Customizer.withDefaults());
         return http.build();
     }
 
--- aad/spring-security/reactive/webflux/oauth2/spring-cloud-gateway/resource-server-2/src/main/java/com/azure/spring/sample/activedirectory/reactive/webflux/oauth2/gateway/resourceserver2/configuration/SecurityConfiguration.java
+++ aad/spring-security/reactive/webflux/oauth2/spring-cloud-gateway/resource-server-2/src/main/java/com/azure/spring/sample/activedirectory/reactive/webflux/oauth2/gateway/resourceserver2/configuration/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.azure.spring.sample.activedirectory.reactive.webflux.oauth2.gateway.resourceserver2.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @EnableWebFluxSecurity
+@Configuration
 public class SecurityConfiguration {
 
 }

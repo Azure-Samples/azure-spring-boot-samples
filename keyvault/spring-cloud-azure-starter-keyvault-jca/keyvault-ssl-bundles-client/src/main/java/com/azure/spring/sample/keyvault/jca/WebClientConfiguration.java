8114c3bca8f4593d4b1fff472d82bbcbcbf1f62d
Xiaolu Dai <xiada@microsoft.com>
2025-03-04 21:20:22 +0800

    refactor

--- keyvault/spring-cloud-azure-starter-keyvault-jca/keyvault-ssl-bundles-client/src/main/java/com/azure/spring/sample/keyvault/jca/WebClientConfiguration.java
+++ keyvault/spring-cloud-azure-starter-keyvault-jca/keyvault-ssl-bundles-client/src/main/java/com/azure/spring/sample/keyvault/jca/WebClientConfiguration.java
@@ -3,55 +3,29 @@
 
 package com.azure.spring.sample.keyvault.jca;
 
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import org.springframework.boot.ssl.SslBundle;
-import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.TrustManagerFactory;
 
 @Configuration
 public class WebClientConfiguration {
 
-    private final SslBundles sslBundles;
-
-    public WebClientConfiguration(SslBundles sslBundles) {
-        this.sslBundles = sslBundles;
+    @Bean
+    public WebClient webClientWithTLS(WebClientSsl ssl) throws Exception {
+        return buildWebClientEnableTls(false, ssl);
     }
 
     @Bean
-    public WebClient webClientWithTLS() throws Exception {
-        return buildWebClientEnableTls(false);
+    public WebClient webClientWithMTLS(WebClientSsl ssl) throws Exception {
+        return buildWebClientEnableTls(true, ssl);
     }
 
-    @Bean
-    public WebClient webClientWithMTLS() throws Exception {
-        return buildWebClientEnableTls(true);
-    }
-
-    private WebClient buildWebClientEnableTls(boolean enableMtls) throws Exception {
-        SslBundle sslBundle = sslBundles.getBundle("clientBundle");
-        KeyManager keyManager = enableMtls ? sslBundle.getManagers().getKeyManagers()[0] : null;
-        TrustManagerFactory trustManagerFactory = InsecureTrustManagerFactory.INSTANCE;
-        SslContext sslContext = SslContextBuilder
-            .forClient()
-            .keyManager(keyManager)
-            .trustManager(trustManagerFactory)
-            .build();
-        HttpClient httpClient = HttpClient.create()
-                                          .secure(sslSpec -> sslSpec.sslContext(sslContext));
-        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+    private WebClient buildWebClientEnableTls(boolean enableMtls, WebClientSsl ssl) throws Exception {
+        String sslBundleName = enableMtls ? "mtlsClientBundle" : "tlsClientBundle";
         return WebClient.builder().baseUrl("https://localhost:8444")
-                        .clientConnector(connector)
-                        .build();
+                .apply(ssl.fromBundle(sslBundleName))
+                .build();
     }
 
 }

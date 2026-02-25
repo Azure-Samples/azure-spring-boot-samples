81b4c41deb51dd894bfd4bf11bb0f84fa515e71e
Rujun Chen <rujche@microsoft.com>
2026-02-25 14:21:37 +0800

    Update test about test container

--- storage/spring-cloud-azure-starter-storage-blob/storage-blob-sample/src/test/java/com/azure/spring/sample/storage/resource/AzureBlobResourceTestContainerTest.java
+++ storage/spring-cloud-azure-starter-storage-blob/storage-blob-sample/src/test/java/com/azure/spring/sample/storage/resource/AzureBlobResourceTestContainerTest.java
@@ -1,4 +1,4 @@
-package com.azure.spring.sample.storage.resource.test;
+package com.azure.spring.sample.storage.resource;
 
 import com.azure.spring.cloud.autoconfigure.implementation.context.AzureGlobalPropertiesAutoConfiguration;
 import com.azure.spring.cloud.autoconfigure.implementation.storage.blob.AzureStorageBlobAutoConfiguration;
@@ -6,11 +6,15 @@ import com.azure.spring.cloud.autoconfigure.implementation.storage.blob.AzureSto
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.util.StreamUtils;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -18,12 +22,15 @@ import java.nio.charset.Charset;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(properties = {
-        "spring.docker.compose.skip.in-tests=false",
-        "spring.docker.compose.file=classpath:storage-compose.yaml",
-        "spring.docker.compose.stop.command=down"
-})
-public class AzureBlobResourceTest {
+@SpringJUnitConfig
+@Testcontainers
+class AzureBlobResourceTestContainerTest {
+    @Container
+    @ServiceConnection
+    private static final GenericContainer<?> AZURITE_CONTAINER = new GenericContainer<>(
+            "mcr.microsoft.com/azure-storage/azurite:latest")
+            .withExposedPorts(10000)
+            .withCommand("azurite --skipApiVersionCheck && azurite -l /data --blobHost 0.0.0.0 --queueHost 0.0.0.0 --tableHost 0.0.0.0");
 
     @Value("azure-blob://testcontainers/message.txt")
     private Resource blobFile;
@@ -39,10 +46,7 @@ public class AzureBlobResourceTest {
     }
 
     @Configuration(proxyBeanMethods = false)
-    @ImportAutoConfiguration(classes = {
-            AzureGlobalPropertiesAutoConfiguration.class,
-            AzureStorageBlobAutoConfiguration.class,
-            AzureStorageBlobResourceAutoConfiguration.class})
+    @ImportAutoConfiguration(classes = {AzureGlobalPropertiesAutoConfiguration.class, AzureStorageBlobAutoConfiguration.class, AzureStorageBlobResourceAutoConfiguration.class})
     static class Config {
     }
 }
